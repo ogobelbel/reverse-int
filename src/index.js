26432ccd4577e481c1943108ab5490e4adff9593
Alexey <ogobel@icloud.com>
2021-02-28 20:25:37 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let a = n.toString().split('');
+  let res = '';
+for(let i = a.length-1; i>=0; i--){
+ if(a[i]=='-'){
+     continue;
+ }
+  res+=a[i];
+}
+return res;
 }
